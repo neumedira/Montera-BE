4d7aa8d00c21530db925de470ac25c4e0f3c6d9e
Muhammad Rizky Syafutra <310123023808@stmikbanjarbaru.id>
2026-08-20 21:12:40 +0800

    nambahin guard di paymentsetting, lupa nambahin

--- app/Models/PaymentSetting.php
+++ app/Models/PaymentSetting.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class PaymentSetting extends Model
 {
-    //
+    use HasFactory;
+
+    protected $guarded = ['id'];
 }
